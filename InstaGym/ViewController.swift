8c6fc16e8ee85e51520b53570a1fa25d39ef4661
Alexandre768 <Alexandredob13@gmail.com>
2023-09-19 21:08:58 -0300

    Primeiro Commit

---
+++ InstaGym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaGym
+//
+//  Created by aluno on 19/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
