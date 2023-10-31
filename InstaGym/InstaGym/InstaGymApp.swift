f41f00ce9459b94d327956ee92e61d7776b83e06
Alexandre <103066821+Alexandre768@users.noreply.github.com>
2023-10-31 20:20:29 -0300

    Add files via upload

---
+++ InstaGym/InstaGym/InstaGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaGymApp.swift
+//  InstaGym
+//
+//  Created by alexandre pereira on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
