f82b8d08f0e82c241969689a30bc85436b9c0dc4
Alexandre768 <alexandredob13@gmail.com>
2023-11-06 19:43:55 -0300

    Novas telas

---
+++ Insta-Gym Version 2/Model/addTreinModel.swift
@@ -0,0 +1,13 @@
+//
+//  addTreinView.swift
+//  InstaGym
+//
+//  Created by alexandre pereira on 23/10/23.
+//
+
+// Adicionar treinos
+import Foundation
+
+var teste = addTrein(id: 1, name: " Costa", imageNAme: "fit")
+  
+
---
+++ Insta-Gym Version 2/Model/description.swift
@@ -0,0 +1,16 @@
+// STRUCT  para descricao da segunda tela
+
+import Foundation
+struct Description{
+    var id: Int
+    var titlePrin: String
+    var titleSeg: String
+    var description: String
+    var videoList: String
+    var textVideo: String
+    var min: String
+    var cal: String
+    var bpm: String
+    
+   
+    }
