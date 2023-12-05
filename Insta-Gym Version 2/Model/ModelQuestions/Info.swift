9626272e6bae25a73e37514d1f2fab418a879eb7
Anne Karolinne Dias <58616039+annekarolinneds@users.noreply.github.com>
2023-12-05 13:04:48 -0300

    Organização dos códigos

---
+++ Insta-Gym Version 2/Model/ModelQuestions/Info.swift
@@ -0,0 +1,13 @@
+//
+//  Info.swift
+//  Insta-Gym Version 2
+//
+//  Created by Anne Karolinne  on 05/12/23.
+//
+
+import SwiftUI
+
+//MARK: Quiz info Codable Model
+struct Info: Codable {
+    var title: String
+}
